0927c5692f79ecc30ac919ae0df2bc95671fbd0f
Adrian Neow <adrianneow96@hotmail.com>
2017-10-09 13:27:56 +0800

    First commit

---
+++ Airline/Controllers/AccountController.cs
@@ -0,0 +1,86 @@
+﻿using Airline.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Airline.Controllers
+{
+    public class AccountController : Controller
+    {
+        
+        UIAirlinesEntities db = new UIAirlinesEntities();
+
+        
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(tblUserAccount model, string returnUrl)
+        {
+    
+            
+            var dataItem = db.tblUserAccounts.Where(x => x.UserName == model.UserName && x.Password == model.Password).FirstOrDefault();
+            if (dataItem != null)
+            {
+                FormsAuthentication.SetAuthCookie(dataItem.UserName, false);
+                if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                         && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                {
+                    Session["loginID"] = dataItem.User_id;
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    Session["loginID"] = dataItem.User_id;
+                    return RedirectToAction("Index","Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid username/password");
+                return View();
+            }
+        }
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(tblUserAccount model)
+        {
+            tblUserAccount tbl = new tblUserAccount();
+
+            if (ModelState.IsValid)
+            {
+                if (db.tblUserAccounts.Any(x => x.UserName == model.UserName))
+                {
+                    ModelState.AddModelError("username", "UserName is not available");
+                }
+                else
+                {
+                    Guid guid = Guid.NewGuid();
+                    tbl.User_id = guid;
+                    tbl.UserName = model.UserName;
+                    tbl.Password = model.Password;
+
+                    db.tblUserAccounts.Add(tbl);
+                    db.SaveChanges();
+                }
+            }
+
+            return RedirectToAction("Login","Account");
+        }
+    }
+}
---
+++ Airline/Controllers/BookingController.cs
@@ -0,0 +1,184 @@
+﻿using Airline.Class;
+using Airline.Models;
+using Airline.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Airline.Controllers
+{
+    public class BookingController : Controller
+    {
+        //
+        // GET: /Booking/
+        UIAirlinesEntities db = new UIAirlinesEntities();
+      
+        public ActionResult Manage(string q)
+        {
+            string str = q;
+            //var item = (from s in db.Flights where s.Origin.StartsWith(str) select s).ToList();
+            var item = db.Flights.ToList();
+            if(!string.IsNullOrWhiteSpace(q))
+            {
+                item = item.Where(p => p.DepartureVenue.ToLower().Contains(q.ToLower())).ToList();
+            }
+            if (item.Count != 0)
+            {
+                return View(item);
+            }
+            else
+            {
+                ViewBag.empty = "empty";
+                return View();
+            }
+        }
+        
+        [Authorize]
+        public ActionResult Book(Guid id)
+        {
+            var FlightID = id;
+            var loginID = Session["loginID"];
+            BookFlightVM item = new BookFlightVM();
+            var data = db.Flights.Where(x => x.Flight_id == FlightID).First();
+
+            item.FlightID = FlightID;
+            item.Origin = data.DepartureVenue;
+            item.Destination = data.DestinationVenue;
+            item.DepartureTime = data.DepartureTime;
+            item.ArrivalTime = data.ArrivalTime;
+            return View(item);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Book(BookFlightVM model)
+        {
+
+            Booking tbl = new Booking();
+            Guid loginID = (Guid)Session["loginID"];
+
+            Guid guid = Guid.NewGuid();
+            tbl.Booking_id = guid;
+            tbl.Flight_id = model.FlightID;
+            tbl.User_id = loginID;
+
+
+
+            var tbl2 = db.Flights.Where(x => x.Flight_id == model.FlightID).First();
+
+            if (tbl2.SeatRemainedNum != 0)
+            {
+                tbl2.SeatRemainedNum = tbl2.SeatRemainedNum - 1;
+                db.SaveChanges();
+                var seat = tbl2.TotalSeatNum - tbl2.SeatRemainedNum;
+
+                if (seat >= 1 && seat <= 3)
+                {
+                    var seatMod = seat % 3;
+                    if(seatMod == 0)
+                    {
+                        seatMod = 3;
+                    }
+                    tbl.SeatNum = "A" + seatMod.ToString();
+                }
+
+                if (seat >= 4 && seat <= 6)
+                {
+                    var seatMod = seat % 3;
+                    if (seatMod == 0)
+                    {
+                        seatMod = 3;
+                    }
+                    tbl.SeatNum = "B" + seatMod.ToString();
+                }
+
+                if (seat >= 7 && seat <= 9)
+                {
+                    var seatMod = seat % 3;
+                    if (seatMod == 0)
+                    {
+                        seatMod = 3;
+                    }
+                    tbl.SeatNum = "C" + seatMod.ToString();
+                }
+
+                if (seat >= 10 && seat <= 12)
+                {
+                    var seatMod = seat % 3;
+                    if (seatMod == 0)
+                    {
+                        seatMod = 3;
+                    }
+                    tbl.SeatNum = "D" + seatMod.ToString();
+                }
+                if (seat >= 13 && seat <= 15)
+                {
+                    var seatMod = seat % 3;
+                    if (seatMod == 0)
+                    {
+                        seatMod = 3;
+                    }
+                    tbl.SeatNum = "E" + seatMod.ToString();
+                }
+                if (seat >= 16 && seat <= 18)
+                {
+                    var seatMod = seat % 3;
+                    if (seatMod == 0)
+                    {
+                        seatMod = 3;
+                    }
+                    tbl.SeatNum = "E" + seatMod.ToString();
+                }
+                if (seat >= 19 && seat <= 20)
+                {
+                    var seatMod = seat % 3;
+                    if (seatMod == 0)
+                    {
+                        seatMod = 3;
+                    }
+                    tbl.SeatNum = "E" + seatMod.ToString();
+                }
+                
+            }
+            db.Bookings.Add(tbl);
+            db.SaveChanges();
+            return RedirectToAction("ViewBooking","Booking");
+        }
+
+        [Authorize]
+        public ActionResult ViewBooking()
+        {
+            
+            Guid userID = (Guid)Session["loginID"]; 
+
+            var data = (from s in db.Bookings
+                        join d in db.Flights on s.Flight_id equals d.Flight_id 
+                        where s.User_id == userID
+                        select new ViewBookingVM
+                        {
+                            Origin = d.DepartureVenue,
+                            Destination = d.DestinationVenue,
+                            DepartureTime = d.DepartureTime,
+                            ArrivalTime = d.ArrivalTime,
+                            Seat = s.SeatNum
+                        }).ToList();
+
+            ListViewBookingVM model = new ListViewBookingVM();
+            model.List1 = data;
+
+            if (data.Count != 0)
+            {
+                return View(model);
+            }
+            else
+            {
+                ViewBag.empty = "empty";
+                    return View();
+            }
+
+
+        }
+    }
+}
